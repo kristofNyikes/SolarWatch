f42d94409f1b0b33df124f4ece1f21588725a52a
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-09-26 15:58:23 +0200

    feat: add Weather and SunriseSunset models

---
+++ SolarWatch/Models/SunriseSunset/SunriseSunset.cs
@@ -0,0 +1,15 @@
+﻿namespace SolarWatch.Models.SunriseSunset;
+
+public class SunriseSunset
+{
+    public DateTime Sunrise { get; set; }
+    public DateTime Sunset { get; set; }
+    public DateTime SolarNoon { get; set; }
+    public TimeSpan DayLength { get; set; }
+    public DateTime CivilTwilightBegin { get; set; }
+    public DateTime CivilTwilightEnd { get; set; }
+    public DateTime NautricalTwilightBegin { get; set; }
+    public DateTime NautricalTwilightEnd { get; set; }
+    public DateTime AstronomicalTwilightBegin { get; set; }
+    public DateTime AstronomicalTwilightEnd { get; set; }
+}
