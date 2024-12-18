c6f82dd8dc6e67e3d7101fc1aa67e7664506f178
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-12-18 10:24:50 +0100

    refactor: delete unused methods

--- SolarWatchApi/Services/Repository/ISunriseSunsetRepository.cs
+++ SolarWatchApi/Services/Repository/ISunriseSunsetRepository.cs
@@ -6,6 +6,4 @@ public interface ISunriseSunsetRepository
 {
     Task<IEnumerable<SunriseSunset>> GetAllAsync();
     Task AddAsync(SunriseSunset sunriseSunset);
-    Task UpdateAsync(SunriseSunset sunriseSunset);
-    Task DeleteAsync(int id);
 }
