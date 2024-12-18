28f0b5c03faca91626375e11f900d497ce76f475
Kristóf Nyikes <kristof.nyikes@gmail.com>
2024-12-18 10:29:59 +0100

    refactor: remove comment

--- SolarWatchApi/Contracts/AuthResponse.cs
+++ SolarWatchApi/Contracts/AuthResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace SolarWatchApi.Contracts;
 
-public record AuthResponse(bool Success, string Email, string UserName/*, IList<string> Role*/);
+public record AuthResponse(bool Success, string Email, string UserName);
